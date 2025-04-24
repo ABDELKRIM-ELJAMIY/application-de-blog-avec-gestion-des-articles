423c5ab47dc34ba33fd0aa44425293a642fef975
didi <eljamiyenaa@gmai.com>
2025-04-24 15:33:52 +0100

    update article details

---
+++ src/pages/ArticleDetails.jsx
@@ -0,0 +1,18 @@
+
+import { Routes, Route } from 'react-router-dom'
+import Article from './compenents/ArticleForm'
+import ArticleCard from './compenents/ArticleCard'
+function ArticleDetails() {
+
+    return (
+
+        <Routes>
+            <Route path='/' element={<ArticleCard />} />
+            <Route path="/AddArticle" element={<Article />} />
+
+        </Routes>
+
+    )
+}
+
+export default ArticleDetails
