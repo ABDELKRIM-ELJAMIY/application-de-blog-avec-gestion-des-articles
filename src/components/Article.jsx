8da3ee0064499bbf46276c71bcb1d0f7e80fcb58
didi <eljamiyenaa@gmai.com>
2025-04-24 10:20:56 +0100

    update

---
+++ src/components/Article.jsx
@@ -0,0 +1,13 @@
+import React ,{useState} from 'react'
+import * as Yup from 'yup';
+
+const Article = () => {
+    const [Image, setImage] = useState(null)
+    const [category, setcategory] = useState('')
+    
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
